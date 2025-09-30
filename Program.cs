c86c3722fbc6172e72c9ca1df2f91f2d87e95789
Malin Hallgren <malin.m.hallgren@gmail.com>
2025-09-30 18:39:17 +0200

    basic setup

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using WordleConsoleApp.Words;
+
+namespace WordleConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var game = new Game();
+            game.SetDirectory();
+
+            var word = new Word();
+        }
+    }
+}
---
+++ Words/Word.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleConsoleApp.Words
+{
+    internal class Word
+    {
+        private string CurrentDirectory;
+        private string[] PrecursorPossibleWords;
+        private List <string> PossibleWords { get; set; } 
+        public string SelectedWord { get; private set; }
+
+        public Word()
+        {
+            CurrentDirectory = Directory.GetCurrentDirectory();
+            PrecursorPossibleWords = File.ReadAllLines(CurrentDirectory + @"\Words\WordLibrary.txt");
+            PossibleWords = PrecursorPossibleWords.ToList();
+            SelectedWord = PickWord();
+        }
+
+        public string PickWord()
+        {
+           
+            
+            foreach (var word in PossibleWords)
+            {
+                Console.WriteLine(word);
+            }
+
+            var random = new Random();
+            int randomIndex = random.Next(PossibleWords.Count);
+
+            Console.WriteLine($"Word picked was: {PossibleWords[randomIndex]}");
+            return PossibleWords[randomIndex];
+        }
+    }
+}
